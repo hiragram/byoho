47f615fb02917614fe20de140589cc4af8670556
Yuya Hirayama <hiragram@gmail.com>
2025-01-21 21:42:52 +0900

    Initialize Swift package

---
+++ Sources/Entrypoint.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  byoho
+//
+//  Created by Yuya Hirayama on 2025/01/21.
+//
+
+import Foundation
+import ArgumentParser
+
+@main
+struct Byoho: AsyncParsableCommand {
+    mutating func run() async throws {
+        print("Hello, world!")
+    }
+}
